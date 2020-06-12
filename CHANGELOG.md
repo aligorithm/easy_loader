485922799b559912e2252c3d78200cf248f6dcd8
Aliyu Yisa <aliyisa@outlook.com>
2020-06-12 16:13:31 +0100

    added example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## [0.0.1] - 12/06/2020
 
 * Initial release
+
+## [0.0.2] - 12/06/2020
+
+* Added example
---
+++ example/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:easy_loader/easy_loader.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //// Wrap your body in a stack
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: Text("Lorem Ipsum"),
+          ),
+          //// Put the loader widget at the end of the stack. You can set it to appear based on a boolean. E.g. a loading flag.
+          EasyLoader()
+        ],
+      ),
+    );
+  }
+}
